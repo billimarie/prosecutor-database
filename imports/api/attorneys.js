80808d971100ae3e705d1f3d23638c79cee2fe59
Rick <shariq1989@gmail.com>
2020-10-04 21:10:06 -0600

    add race to attorney schema

--- imports/api/attorneys.js
+++ imports/api/attorneys.js
@@ -1,42 +1,47 @@
-import { Mongo } from 'meteor/mongo';
+import {Mongo} from 'meteor/mongo';
 
 import SimpleSchema from 'simpl-schema';
 
-export const Attorneys = new Mongo.Collection( 'Attorneys' );
+export const Attorneys = new Mongo.Collection('Attorneys');
 
-if ( Meteor.isServer ) {
-  Attorneys._ensureIndex( {
-    name: 1,
-    role: 1,
-    state: 1
-  });
+if (Meteor.isServer) {
+    Attorneys._ensureIndex({
+        name: 1,
+        role: 1,
+        state: 1
+    });
 }
 
 Attorneys.allow({
-  insert: () => false,
-  update: () => false,
-  remove: () => false
+    insert: () => false,
+    update: () => false,
+    remove: () => false
 });
 
 Attorneys.deny({
-  insert: () => true,
-  update: () => true,
-  remove: () => true
+    insert: () => true,
+    update: () => true,
+    remove: () => true
 });
 
 let AttorneysSchema = new SimpleSchema({
-  'name': {
-    type: String,
-    label: 'The name of this attorney.'
-  },
-  'role': {
-    type: String,
-    label: 'The role of this attorney.'
-  },
-  'state': {
-    type: String,
-    label: 'The state of this attorney.'
-  }
+    'name': {
+        type: String,
+        label: 'The name of this attorney.'
+    },
+    'role': {
+        type: String,
+        label: 'The role of this attorney.'
+    },
+    'state': {
+        type: String,
+        label: 'The state of this attorney.'
+    },
+    'race': {
+        type: String,
+        label: 'The race of this attorney.'
+    },
+
 });
 
-Attorneys.attachSchema( AttorneysSchema );
+Attorneys.attachSchema(AttorneysSchema);
