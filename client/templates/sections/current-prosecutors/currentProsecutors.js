3f46edce1c2ca8a04c1c8d73814f5889af01fa57
Rick <shariq1989@gmail.com>
2020-10-04 20:34:56 -0600

    first jab at creating race filter

--- client/templates/sections/current-prosecutors/currentProsecutors.js
+++ client/templates/sections/current-prosecutors/currentProsecutors.js
@@ -1,7 +1,7 @@
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
+import {Template} from 'meteor/templating';
+import {ReactiveDict} from 'meteor/reactive-dict';
 
-import { Attorneys } from '../../../../imports/api/attorneys.js';
+import {Attorneys} from '../../../../imports/api/attorneys.js';
 import './current-prosecutors.html';
 
 Template.currentProsecutors.onCreated(function () {
@@ -20,10 +20,20 @@ Template.currentProsecutors.helpers({
             'Municipal Attorney'
         ]
     },
+    raceFilters() {
+        return [
+            'American Indian',
+            'Asian',
+            'Black',
+            'Hispanic',
+            'Pacific Islander',
+            'White'
+        ]
+    },
     attorneys() {
         const selectedRoleFilters = Template.instance().state.get("selectedRoleFilters")
         return selectedRoleFilters.length > 0
-            ? Attorneys.find({ "role": { $in: selectedRoleFilters } }).fetch()
+            ? Attorneys.find({"role": {$in: selectedRoleFilters}}).fetch()
             : Attorneys.find().fetch()
     },
 });
@@ -36,5 +46,13 @@ Template.currentProsecutors.events({
             ? selectedRoleFilters.filter((value) => value !== filter)
             : [...selectedRoleFilters, filter]
         instance.state.set("selectedRoleFilters", newSelectedRoleFilters)
+    },
+    "change .race-filter"(event, instance) {
+        const filter = event.currentTarget.name
+        const selectedRaceFilters = instance.state.get("selectedRaceFilters")
+        const newSelectedRaceFilters = selectedRaceFilters.includes(filter)
+            ? selectedRaceFilters.filter((value) => value !== filter)
+            : [...selectedRaceFilters, filter]
+        instance.state.set("selectedRaceFilters", newSelectedRaceFilters)
     }
 })
